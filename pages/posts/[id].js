84f018be20a592dccc064adc26b46b47e4646881
Clemy-beep <clementine.digny@gmail.com>
2022-03-17 09:58:12 +0100

    article page added

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Date from "../../components/date";
+import Layout from "../../components/layout";
+import { getAllPostsIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+export async function getStaticPaths() {
+  const paths = getAllPostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head> {postData.title}</Head>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+    </Layout>
+  );
+}
